267586448ea404e7847bb59bae15a2342649edc3
Chetan <chetankumarsahu2525@gmail.com>
2024-06-29 17:45:04 +0530

    add files to the repo

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const mail = document.getElementById("email");
+const subject = document.getElementById("subject");
+const userMessage = document.getElementById("message");
+
+
+function sendEmail(){
+    const messageBody = `Name: ${fullName.value} <br> Email: ${mail.value} <br> Message: ${userMessage.value}`;
+    // console.log(fullName.value, userMessage.value, mail.value);
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "chandupopat560@gmail.com",
+        Password : "CE3832802E538D66A6282B120E627956A01C",
+        To : "chandupopat560@gmail.com",
+        From : "chandupopat560@gmail.com",
+        Subject : subject.value,
+        Body : messageBody
+    }).then(
+        
+        message => alert(message),
+        console.log("message has been sent")
+    );
+
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+})
